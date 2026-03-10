88c464f43477af1b86ed570c3d9c953337c61689
Aldris <aldrislostboy@gmail.com>
2026-03-10 11:36:33 +0100

    fixes

--- Form2.cs
+++ Form2.cs
@@ -13,30 +13,45 @@ namespace Strooptest
         private Button btnZurueck;
         private Panel gamePanel;
         private Label lblWort;
+        private Label lblPunkte;
+        private Label lblLetzteAntwort;
 
-        private string[] woerter = { "Rot", "Blau", "Gelb", "Schwarz", "Weiß" };
-        private Color[] farben = { Color.Red, Color.Blue, Color.Yellow, Color.Black, Color.White };
+        // Wort- und Farbarrays
+        private string[] woerter = { "Rot", "Blau", "Gelb", "Grün", "Orange", "Lila", "Pink", "Braun", "Cyan" };
+        private Color[] farben = {
+            Color.Red,      // Rot
+            Color.Blue,     // Blau
+            Color.Yellow,   // Gelb
+            Color.Green,    // Grün
+            Color.Orange,   // Orange
+            Color.Purple,   // Lila
+            Color.HotPink,  // Pink
+            Color.Brown,    // Braun
+            Color.Cyan      // Cyan
+        };
 
         private Random random = new Random();
         private bool warningShown = false;
+        private int punkte = 0;
 
         public Form2()
         {
             InitializeComponent();
             InitializeGameComponents();
         }
+
         private void Form2_Load(object sender, EventArgs e)
         {
             // Hier muss nichts rein
         }
 
-
         private void InitializeGameComponents()
         {
             this.Text = "Stroop Test - Spiel";
-            this.Size = new Size(650, 750);
+            this.Size = new Size(650, 800);
             this.StartPosition = FormStartPosition.CenterScreen;
 
+            // Zurück-Button
             btnZurueck = new Button
             {
                 Text = "← Zurück zum Hauptmenü",
@@ -47,65 +62,95 @@ namespace Strooptest
             btnZurueck.Click += (s, e) => this.Close();
             this.Controls.Add(btnZurueck);
 
+            // Punkte-Anzeige
+            lblPunkte = new Label
+            {
+                Text = "Punkte: 0",
+                Location = new Point(500, 20),
+                Size = new Size(120, 30),
+                Font = new Font("Arial", 14, FontStyle.Bold),
+                TextAlign = ContentAlignment.MiddleRight,
+                ForeColor = Color.DarkGreen
+            };
+            this.Controls.Add(lblPunkte);
+
+            // Letzte Antwort-Anzeige
+            lblLetzteAntwort = new Label
+            {
+                Text = "",
+                Location = new Point(20, 55),
+                Size = new Size(600, 25),
+                Font = new Font("Arial", 10, FontStyle.Italic),
+                TextAlign = ContentAlignment.MiddleCenter,
+                ForeColor = Color.Gray
+            };
+            this.Controls.Add(lblLetzteAntwort);
+
+            // Wort-Label oben
             lblWort = new Label
             {
                 Text = "Rot",
-                Location = new Point(20, 60),
-                Size = new Size(550, 50),
-                Font = new Font("Arial", 24, FontStyle.Bold),
+                Location = new Point(20, 85),
+                Size = new Size(550, 60),
+                Font = new Font("Arial", 28, FontStyle.Bold),
                 TextAlign = ContentAlignment.MiddleCenter,
                 BackColor = Color.LightYellow,
                 BorderStyle = BorderStyle.FixedSingle
             };
             this.Controls.Add(lblWort);
 
+            // Game Panel
             gamePanel = new Panel
             {
-                Location = new Point(20, 120),
+                Location = new Point(20, 155),
                 Size = new Size(550, 520),
                 BorderStyle = BorderStyle.FixedSingle,
                 BackColor = Color.White
             };
             this.Controls.Add(gamePanel);
 
+            // GameBoard initialisieren
             gameBoard = new StroopNEinzelspieler(gamePanel);
-            gameBoard.PictureBoxClicked += GameBoard_PictureBoxClicked;
+            gameBoard.CorrectAnswerSelected += GameBoard_CorrectAnswerSelected;
+            gameBoard.WrongAnswerSelected += GameBoard_WrongAnswerSelected;
 
+            // Anzahl PictureBoxes Label
             Label lblAnzahl = new Label
             {
                 Text = "Anzahl PictureBoxes:",
-                Location = new Point(220, 660),
+                Location = new Point(220, 695),
                 Size = new Size(130, 25)
             };
             this.Controls.Add(lblAnzahl);
 
+            // NumericUpDown für Anzahl
             nudAnzahlPictureBoxes = new NumericUpDown
             {
-                Location = new Point(350, 660),
+                Location = new Point(350, 695),
                 Size = new Size(80, 25),
                 Minimum = 1,
-                Maximum = 100, // höher setzen!
+                Maximum = 100,
                 Value = 9
             };
-
             nudAnzahlPictureBoxes.ValueChanged += NudAnzahlPictureBoxes_ValueChanged;
             this.Controls.Add(nudAnzahlPictureBoxes);
 
+            // Grid Erstellen Button
             btnGridErstellen = new Button
             {
                 Text = "Grid erstellen",
-                Location = new Point(440, 658),
+                Location = new Point(440, 693),
                 Size = new Size(120, 30),
                 BackColor = Color.LightGreen
             };
             btnGridErstellen.Click += BtnGridErstellen_Click;
             this.Controls.Add(btnGridErstellen);
 
+            // Neues Spiel starten
             gameBoard.AnzahlPictureBoxes = 9;
-            AendereWort();
+            StarteNeueRunde();
         }
 
-        // 🔴 HIER kommt deine gewünschte MessageBox
         private void NudAnzahlPictureBoxes_ValueChanged(object sender, EventArgs e)
         {
             if (nudAnzahlPictureBoxes.Value > 40)
@@ -113,16 +158,14 @@ namespace Strooptest
                 if (!warningShown)
                 {
                     warningShown = true;
-
                     MessageBox.Show(
                         "Die Anzahl der PictureBoxes darf 40 nicht überschreiten!\n" +
                         "Bitte wählen Sie einen Wert zwischen 1 und 40.",
-                        "Bro chill mal",
+                        "Hinweis",
                         MessageBoxButtons.OK,
                         MessageBoxIcon.Warning
                     );
                 }
-
                 nudAnzahlPictureBoxes.Value = 40;
                 warningShown = false;
             }
@@ -131,31 +174,63 @@ namespace Strooptest
         private void BtnGridErstellen_Click(object sender, EventArgs e)
         {
             gameBoard.AnzahlPictureBoxes = (int)nudAnzahlPictureBoxes.Value;
+            punkte = 0;
+            lblPunkte.Text = "Punkte: 0";
+            StarteNeueRunde();
         }
 
-        private void AendereWort()
+        private void StarteNeueRunde()
         {
+            // Neues Wort oben generieren
             int wortIndex = random.Next(woerter.Length);
-            lblWort.Text = woerter[wortIndex];
-
             int farbenIndex = random.Next(farben.Length);
+
+            lblWort.Text = woerter[wortIndex];
             lblWort.ForeColor = farben[farbenIndex];
 
-            lblWort.BackColor = lblWort.ForeColor == Color.White
-                ? Color.DarkGray
-                : Color.LightYellow;
+            // Hintergrundfarbe anpassen für bessere Lesbarkeit
+            lblWort.BackColor = (farben[farbenIndex] == Color.White ||
+                                farben[farbenIndex] == Color.Yellow ||
+                                farben[farbenIndex] == Color.HotPink)
+                                ? Color.DarkGray
+                                : Color.LightYellow;
+
+            // Dem GameBoard mitteilen, welches Wort gesucht wird (basierend auf der Farbe des oberen Wortes)
+            gameBoard.SetzeGesuchtesWort(farben[farbenIndex]);
+
+            // Grid mit neuen Wörtern füllen
+            gameBoard.GeneriereAlleLabelsNeu();
         }
 
-        private void AenderePictureBoxFarbe(PictureBox pictureBox)
+        private void GameBoard_CorrectAnswerSelected(object sender, AnswerEventArgs e)
         {
-            int farbenIndex = random.Next(farben.Length);
-            pictureBox.BackColor = farben[farbenIndex];
+            // Punkt erhöhen
+            punkte++;
+            lblPunkte.Text = $"Punkte: {punkte}";
+
+            // Letzte Antwort anzeigen
+            lblLetzteAntwort.Text = $"✓ Richtig! Das Wort '{e.GeklicktesWort}' war gesucht (+1 Punkt)";
+            lblLetzteAntwort.ForeColor = Color.Green;
+
+            // Neue Runde starten
+            StarteNeueRunde();
         }
 
-        private void GameBoard_PictureBoxClicked(object sender, PictureBoxClickEventArgs e)
+        private void GameBoard_WrongAnswerSelected(object sender, AnswerEventArgs e)
         {
-            AendereWort();
-            AenderePictureBoxFarbe(e.ClickedPictureBox);
+            // Letzte Antwort anzeigen
+            lblLetzteAntwort.Text = $"✗ Falsch! Gesucht war '{e.GesuchtesWort}', aber du hast '{e.GeklicktesWort}' geklickt";
+            lblLetzteAntwort.ForeColor = Color.Red;
+
+            // Keine neue Runde, aber kurze Pause zur Visualisierung
+            System.Threading.Tasks.Task.Delay(500).ContinueWith(_ =>
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    // Neue Runde starten
+                    StarteNeueRunde();
+                });
+            });
         }
     }
 }
--- StroopNEinzelspieler.cs
+++ StroopNEinzelspieler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace Strooptest
@@ -12,8 +13,26 @@ namespace Strooptest
         private List<PictureBox> pictureBoxes = new List<PictureBox>();
         private Random rand = new Random();
 
+        // Wort- und Farbarrays 
+        public string[] woerter = { "Rot", "Blau", "Gelb", "Grün", "Orange", "Lila", "Pink", "Braun", "Cyan" };
+        public Color[] farben = {
+            Color.Red,      // Rot
+            Color.Blue,     // Blau
+            Color.Yellow,   // Gelb
+            Color.Green,    // Grün
+            Color.Orange,   // Orange
+            Color.Purple,   // Lila
+            Color.HotPink,  // Pink
+            Color.Brown,    // Braun
+            Color.Cyan      // Cyan
+        };
 
-        public event EventHandler<PictureBoxClickEventArgs> PictureBoxClicked;
+        // Speichert die aktuell gesuchte Wortbedeutung (basierend auf der Farbe des oberen Wortes)
+        private string gesuchtesWort;
+
+        // Events für richtige/falsche Antworten
+        public event EventHandler<AnswerEventArgs> CorrectAnswerSelected;
+        public event EventHandler<AnswerEventArgs> WrongAnswerSelected;
 
         public StroopNEinzelspieler(Panel panel)
         {
@@ -25,7 +44,6 @@ namespace Strooptest
             get { return anzahlPictureBoxes; }
             set
             {
-                // Prüfung auf maximal 40 PictureBoxes
                 if (value > 40)
                 {
                     throw new ArgumentOutOfRangeException(
@@ -81,36 +99,246 @@ namespace Strooptest
             {
                 Location = new Point(startX + (spalte * groesse), startY + (zeile * groesse)),
                 Size = new Size(groesse - 4, groesse - 4),
-                BackColor = GetRandomColor(),
                 BorderStyle = BorderStyle.FixedSingle,
                 SizeMode = PictureBoxSizeMode.StretchImage,
-                Tag = index
+                Tag = index,
+                BackColor = Color.White // Weißer Hintergrund für die Kästchen
             };
 
+            Label lblWort = new Label
+            {
+                Text = "",
+                ForeColor = Color.Black,
+                Font = new Font("Arial", 12, FontStyle.Bold),
+                TextAlign = ContentAlignment.MiddleCenter,
+                Dock = DockStyle.Fill,
+                BackColor = Color.Transparent
+            };
+
+            pb.Controls.Add(lblWort);
             pb.Click += PictureBox_Click;
+            lblWort.Click += (s, e) => PictureBox_Click(pb, e);
+
             return pb;
         }
 
-        private Color GetRandomColor()
+        /// <summary>
+        /// Setzt die gesuchte Wortbedeutung basierend auf der Farbe des oberen Wortes
+        /// </summary>
+        public void SetzeGesuchtesWort(Color farbeDesOberenWortes)
         {
-            return Color.FromArgb(
-                rand.Next(256),
-                rand.Next(256),
-                rand.Next(256)
-            );
+            // Übersetze die Farbe in das entsprechende Wort
+            if (farbeDesOberenWortes == Color.Red)
+                gesuchtesWort = "Rot";
+            else if (farbeDesOberenWortes == Color.Blue)
+                gesuchtesWort = "Blau";
+            else if (farbeDesOberenWortes == Color.Yellow)
+                gesuchtesWort = "Gelb";
+            else if (farbeDesOberenWortes == Color.Green)
+                gesuchtesWort = "Grün";
+            else if (farbeDesOberenWortes == Color.Orange)
+                gesuchtesWort = "Orange";
+            else if (farbeDesOberenWortes == Color.Purple)
+                gesuchtesWort = "Lila";
+            else if (farbeDesOberenWortes == Color.HotPink)
+                gesuchtesWort = "Pink";
+            else if (farbeDesOberenWortes == Color.Brown)
+                gesuchtesWort = "Braun";
+            else if (farbeDesOberenWortes == Color.Cyan)
+                gesuchtesWort = "Cyan";
         }
 
+        /// <summary>
+        /// Generiert ALLE Labels im Grid NEU
+        /// </summary>
+        public void GeneriereAlleLabelsNeu()
+        {
+            int spalten = (int)Math.Ceiling(Math.Sqrt(anzahlPictureBoxes));
+
+            for (int i = 0; i < pictureBoxes.Count; i++)
+            {
+                PictureBox pb = pictureBoxes[i];
+                if (pb.Controls.Count > 0 && pb.Controls[0] is Label lbl)
+                {
+                    int zeile = i / spalten;
+                    int spalte = i % spalten;
+
+                    var (neuesWort, neueFarbe) = GeneriereWortFarbeFuerIndex(i, zeile, spalte);
+
+                    lbl.Text = neuesWort;
+                    lbl.ForeColor = neueFarbe;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Generiert ein Wort-Farbe Paar für einen bestimmten Index
+        /// </summary>
+        private (string wort, Color farbe) GeneriereWortFarbeFuerIndex(int index, int zeile, int spalte)
+        {
+            string ausgewaehltesWort = null;
+            Color ausgewaehlteFarbe = Color.Empty;
+            bool gueltigeKombinationGefunden = false;
+            int maxVersuche = 100;
+            int versuche = 0;
+
+            var nachbarn = ErmittleNachbarn(index, zeile, spalte);
+
+            var verwendeteWoerterInNachbarschaft = new HashSet<string>();
+            var verwendeteFarbenInNachbarschaft = new HashSet<Color>();
+
+            foreach (int nachbarIndex in nachbarn)
+            {
+                if (nachbarIndex >= 0 && nachbarIndex < pictureBoxes.Count)
+                {
+                    PictureBox nachbarPb = pictureBoxes[nachbarIndex];
+                    if (nachbarPb.Controls.Count > 0 && nachbarPb.Controls[0] is Label nachbarLbl)
+                    {
+                        verwendeteWoerterInNachbarschaft.Add(nachbarLbl.Text);
+                        verwendeteFarbenInNachbarschaft.Add(nachbarLbl.ForeColor);
+                    }
+                }
+            }
+
+            while (!gueltigeKombinationGefunden && versuche < maxVersuche)
+            {
+                // Zufälliges Wort auswählen
+                ausgewaehltesWort = woerter[rand.Next(woerter.Length)];
+
+                // Zufällige Farbe auswählen
+                ausgewaehlteFarbe = farben[rand.Next(farben.Length)];
+
+                // Prüfung 1: Wort darf nicht der Farbe entsprechen (Stroop-Effekt)
+                bool wortEntsprichtFarbe = UeberpruefeObWortFarbeEntspricht(ausgewaehltesWort, ausgewaehlteFarbe);
+
+                // Prüfung 2: Weder Wort noch Farbe dürfen in Nachbarfeldern vorkommen
+                bool wortInNachbarschaft = verwendeteWoerterInNachbarschaft.Contains(ausgewaehltesWort);
+                bool farbeInNachbarschaft = verwendeteFarbenInNachbarschaft.Contains(ausgewaehlteFarbe);
+
+                if (!wortEntsprichtFarbe && !wortInNachbarschaft && !farbeInNachbarschaft)
+                {
+                    gueltigeKombinationGefunden = true;
+                }
+
+                versuche++;
+            }
+
+            // Falls keine perfekte Kombination gefunden wurde
+            if (!gueltigeKombinationGefunden)
+            {
+                versuche = 0;
+                while (!gueltigeKombinationGefunden && versuche < maxVersuche)
+                {
+                    ausgewaehltesWort = woerter[rand.Next(woerter.Length)];
+                    ausgewaehlteFarbe = farben[rand.Next(farben.Length)];
+
+                    if (!UeberpruefeObWortFarbeEntspricht(ausgewaehltesWort, ausgewaehlteFarbe))
+                    {
+                        gueltigeKombinationGefunden = true;
+                    }
+                    versuche++;
+                }
+            }
+
+            // Absoluter Notfallplan
+            if (!gueltigeKombinationGefunden)
+            {
+                return (woerter[0], farben[1]);
+            }
+
+            return (ausgewaehltesWort, ausgewaehlteFarbe);
+        }
+
+        private List<int> ErmittleNachbarn(int index, int zeile, int spalte)
+        {
+            var nachbarn = new List<int>();
+            int spalten = (int)Math.Ceiling(Math.Sqrt(anzahlPictureBoxes));
+
+            // Oben
+            if (zeile > 0)
+            {
+                int obenIndex = index - spalten;
+                if (obenIndex >= 0 && obenIndex < anzahlPictureBoxes)
+                    nachbarn.Add(obenIndex);
+            }
+
+            // Unten
+            if (zeile < spalten - 1)
+            {
+                int untenIndex = index + spalten;
+                if (untenIndex >= 0 && untenIndex < anzahlPictureBoxes)
+                    nachbarn.Add(untenIndex);
+            }
+
+            // Links
+            if (spalte > 0)
+            {
+                int linksIndex = index - 1;
+                if (linksIndex >= 0 && linksIndex < anzahlPictureBoxes)
+                    nachbarn.Add(linksIndex);
+            }
+
+            // Rechts
+            if (spalte < spalten - 1)
+            {
+                int rechtsIndex = index + 1;
+                if (rechtsIndex >= 0 && rechtsIndex < anzahlPictureBoxes)
+                    nachbarn.Add(rechtsIndex);
+            }
+
+            return nachbarn;
+        }
+
+        private bool UeberpruefeObWortFarbeEntspricht(string wort, Color farbe)
+        {
+            // Mapping von Wort zu Farbe
+            if (wort == "Rot" && farbe == Color.Red) return true;
+            if (wort == "Blau" && farbe == Color.Blue) return true;
+            if (wort == "Gelb" && farbe == Color.Yellow) return true;
+            if (wort == "Grün" && farbe == Color.Green) return true;
+            if (wort == "Orange" && farbe == Color.Orange) return true;
+            if (wort == "Lila" && farbe == Color.Purple) return true;
+            if (wort == "Pink" && farbe == Color.HotPink) return true;
+            if (wort == "Braun" && farbe == Color.Brown) return true;
+            if (wort == "Cyan" && farbe == Color.Cyan) return true;
+
+            return false;
+        }
 
         private void PictureBox_Click(object sender, EventArgs e)
         {
             PictureBox clickedPb = sender as PictureBox;
             if (clickedPb != null && clickedPb.Tag is int index)
             {
-                PictureBoxClicked?.Invoke(this, new PictureBoxClickEventArgs
+                if (clickedPb.Controls.Count > 0 && clickedPb.Controls[0] is Label lbl)
                 {
-                    Index = index,
-                    ClickedPictureBox = clickedPb
-                });
+                    // Prüfe ob das angeklickte Wort dem gesuchten Wort entspricht
+                    bool istRichtig = (lbl.Text == gesuchtesWort);
+
+                    // Löse das entsprechende Event aus
+                    if (istRichtig)
+                    {
+                        CorrectAnswerSelected?.Invoke(this, new AnswerEventArgs
+                        {
+                            Index = index,
+                            IstRichtig = true,
+                            GeklicktesWort = lbl.Text,
+                            GeklickteFarbe = lbl.ForeColor,
+                            GesuchtesWort = gesuchtesWort
+                        });
+                    }
+                    else
+                    {
+                        WrongAnswerSelected?.Invoke(this, new AnswerEventArgs
+                        {
+                            Index = index,
+                            IstRichtig = false,
+                            GeklicktesWort = lbl.Text,
+                            GeklickteFarbe = lbl.ForeColor,
+                            GesuchtesWort = gesuchtesWort
+                        });
+                    }
+                }
             }
         }
 
@@ -129,14 +357,6 @@ namespace Strooptest
             return pictureBoxes.Count;
         }
 
-        public void SetzePictureBoxBild(int index, Image bild)
-        {
-            if (index >= 0 && index < pictureBoxes.Count)
-            {
-                pictureBoxes[index].Image = bild;
-            }
-        }
-
         public void SetzePictureBoxFarbe(int index, Color farbe)
         {
             if (index >= 0 && index < pictureBoxes.Count)
@@ -146,9 +366,12 @@ namespace Strooptest
         }
     }
 
-    public class PictureBoxClickEventArgs : EventArgs
+    public class AnswerEventArgs : EventArgs
     {
         public int Index { get; set; }
-        public PictureBox ClickedPictureBox { get; set; }
+        public bool IstRichtig { get; set; }
+        public string GeklicktesWort { get; set; }
+        public Color GeklickteFarbe { get; set; }
+        public string GesuchtesWort { get; set; }
     }
 }
